97aef2b989918e169c61509d628c7574497345c2
yaredghebre <yared2391@gmail.com>
2023-07-26 11:33:49 -0300

    Added: Conditional Rendering using function

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,11 +1,18 @@
 // import { Fragment } from "react";
 
 function ListGroup() {
-  const items = ["Roma", "Milano", "Venezia", "Napoli"];
+  let items = ["Roma", "Milano", "Venezia", "Napoli"];
+  items = [];
+
+  // Using a conditional rendering, in JSX recall the function with {}
+  const getMessage = () => {
+    return items.length === 0 ? <p>No cities found</p> : null;
+  };
 
   return (
     <>
       <h1>List</h1>
+      {getMessage()}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
