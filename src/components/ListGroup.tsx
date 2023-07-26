78e89bee36e62337a31f05bf936c2e62ffc194c9
yaredghebre <yared2391@gmail.com>
2023-07-26 11:38:51 -0300

    Add: Condition using && instead of ternary op

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -5,14 +5,18 @@ function ListGroup() {
   items = [];
 
   // Using a conditional rendering, in JSX recall the function with {}
-  const getMessage = () => {
-    return items.length === 0 ? <p>No cities found</p> : null;
-  };
+  //   const getMessage = () => {
+  //     return items.length === 0 ? <p>No cities found</p> : null;
+  //   };
+
+  // Implementation using && instead of ternary:
+  //    - if condition is true, the element after the && will be rendered
+  //    - if condition is false, the whole expression won't be rendered
 
   return (
     <>
       <h1>List</h1>
-      {getMessage()}
+      {items.length === 0 && <p>No cities found</p>}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
